2eb9d16013f4b77a4b51b857c4cba3e0f69f3f3b
lewellin <lewellin@bk.ru>
2020-08-02 11:39:20 +0500

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    var mixer = mixitup('.products__inner-box')
+    // var mixer = mixitup('.products__inner-box')
 
     $(".rate-star").rateYo({
         rating: 5,
@@ -13,4 +13,27 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4,
     });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+
+    $('.icon-th-large').on('click', function(){
+        $('.product__item').removeClass('list');
+        $('.icon-th-list').removeClass('active');
+        $('.icon-th-large').addClass('active');
+    });
+
+
 });
